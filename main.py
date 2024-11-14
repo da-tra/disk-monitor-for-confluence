390c43be0740cd3124b09ca23c48880f5848b2af
da-tra <dtraska@gmail.com>
2024-11-14 15:00:38 +0100

    create a function that adds info about all drives to a list

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ existing Confluence page inside a generated table.
 
 import shutil
 import configuration
+from pprint import pprint
 
 def check_disk_usage(path: str) -> dict:
     disk_dict = dict()
@@ -18,5 +19,27 @@ def check_disk_usage(path: str) -> dict:
     disk_dict["total GB"] = total_gb
     return disk_dict
 
-print(check_disk_usage("/"))
+def update_disks_list(
+    disk_paths: list = configuration.drive_paths,
+    disk_registry: list = disks,
+):
+    """Create a dictionary for each storage device and add it to the disk register ."""
 
+    for disk_path in disk_paths:
+        disk_dict = {}
+        disk_dict["path"] = disk_path
+        disk_dict["storage"] = check_disk_usage(disk_path)
+        disk_registry.append(disk_dict)
+
+# create a list where you can save the information about all the drives
+disks = []
+
+# update the list 'disks' with information about all drives (stored in dictionaries)
+update_disks_list(disk_paths=disks)
+
+
+print("{:.2f}".format(disks[0]["storage"]["used %"]))
+# print(disks[1])
+# print(type(disks[1]))
+# print(type(check_disk_usage("/")))
+# print(type(update_disks_list()))
