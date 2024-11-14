0f95ed846233055daaf4caadb2b3d535b4b6dcb7
da-tra <dtraska@gmail.com>
2024-11-14 13:26:25 +0100

    stop tracking changes in confuguration file

--- configuration.py
+++
@@ -1,8 +0,0 @@
-api_key = ""                    # you Confluence API key a string
-confluence_username = ""        # your Confluence username as an e-mail address
-confluence_website_name = ""    # the domain of your Confluence space as a 
-                                # string("https://...atlassian.net/")
-confluence_page_name = ""       # The name of your existing Confluence page (will be overwritten)
-confluence_page_id = int("")    # the id of your existing confluence page indicated above by name (id is of type int)
-
-drive_paths = []                # a list[str] of the paths of all drives you wish to monitor 
