9225fa1f23d32d0fe73911d36d6a51279bf8d4e4
Lukas Plank <lupl@tuta.io>
2024-11-15 07:26:46 +0100

    refactor: use dataclasses over of raw dicts and avoid side-effects

--- main.py
+++ main.py
@@ -1,45 +1,52 @@
-"""
-Gets data about free storage on mounted drives and HTTP PUTs them on an 
-existing Confluence page inside a generated table.
-"""
+"""Gets data about free storage on mounted drives and HTTP PUTs them on an existing Confluence page inside a generated table."""
 
+from dataclasses import dataclass
 import shutil
-import configuration
-from pprint import pprint
-
-def check_disk_usage(path: str) -> dict:
-    disk_dict = dict()
-    total, used, free = shutil.disk_usage(path)
-    total_gb = total / 2**30
-    used_percent = used / total * 100
-    free_gb = free / 2**30
-
-    disk_dict["used %"] = used_percent
-    disk_dict["free GB"] = free_gb
-    disk_dict["total GB"] = total_gb
-    return disk_dict
-
-def update_disks_list(
-    disk_paths: list = configuration.drive_paths,
-    disk_registry: list = disks,
-):
-    """Create a dictionary for each storage device and add it to the disk register ."""
-
-    for disk_path in disk_paths:
-        disk_dict = {}
-        disk_dict["path"] = disk_path
-        disk_dict["storage"] = check_disk_usage(disk_path)
-        disk_registry.append(disk_dict)
-
-# create a list where you can save the information about all the drives
-disks = []
-
-# update the list 'disks' with information about all drives (stored in dictionaries)
-update_disks_list(disk_paths=disks)
 
 
-print("{:.2f}".format(disks[0]["storage"]["used %"]))
-# print(disks[1])
-# print(type(disks[1]))
-# print(type(check_disk_usage("/")))
-# print(type(update_disks_list()))
+class configuration:
+    drive_paths = ["."]
+
+
+@dataclass
+class DiskUsageInfo:
+    total_gb: int
+    used_percent: float
+    free_gb: int
+
+
+@dataclass
+class DriveInfo:
+    path: str
+    storage: DiskUsageInfo
+
+
+def get_disk_usage(path: str) -> DiskUsageInfo:
+    _total, _used, _free = shutil.disk_usage(path)
+
+    total_gb = _total / 2**30
+    used_percent = _used / _total * 100
+    free_gb = _free / 2**30
+
+    disk_usage_info = DiskUsageInfo(
+        total_gb=total_gb, used_percent=used_percent, free_gb=free_gb
+    )
+
+    return disk_usage_info
+
+
+def generate_drive_registry(drive_paths: list[str]) -> list[DriveInfo]:
+    registry = [
+        DriveInfo(path=path, storage=get_disk_usage(path)) for path in drive_paths
+    ]
+
+    return registry
+
+
+disk_registry: list[DriveInfo] = generate_drive_registry(
+    drive_paths=configuration.drive_paths
+)
+
+print("{:.2f}".format(disk_registry[0].storage.used_percent))
+print()
+print(disk_registry[0])
