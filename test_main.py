54036918c0c52c8520b0ba1c9d7ad3b9a86b2092
da-tra <dtraska@gmail.com>
2024-11-24 18:11:25 +0100

    create test file and start writing test code

---
+++ test_main.py
@@ -0,0 +1,4 @@
+from main import DriveInfo, DiskUsageInfo, create_disks_list, create_table_html
+
+def test_create_table_html():
+    """Does the HTML table created from dataclass DriveInfo match the one created from dictionaries?"""
