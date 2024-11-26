9bfd6dbb43a9e22729676e67e04f77157a72f093
da-tra <dtraska@gmail.com>
2024-11-26 14:17:41 +0100

    modify test function to check for all items in configuration.drive_paths via loop

--- test_main.py
+++ test_main.py
@@ -3,14 +3,16 @@ from main import create_disks_list, create_drive_registry
 from main import create_table_html # , create_table_html_dc
 import configuration
 
-def test_dclass_diskusageinfo(path=configuration.drive_paths[0]):
+def test_dclass_diskusageinfo(paths=configuration.drive_paths):
     """Does the dataclass based check_disk_usage_dc obtain the same data as check_disk_usage?"""
-    dataclass = check_disk_usage_dc(path)
-    function = check_disk_usage(path)
+    
+    for path in paths:
+        dataclass = check_disk_usage_dc(path)
+        function = check_disk_usage(path)
 
-    assert dataclass.used_percent == function["used %"]
-    assert dataclass.free_gb == function["free GB"]
-    assert dataclass.total_gb == function["total GB"]
+        assert dataclass.used_percent == function["used %"]
+        assert dataclass.free_gb == function["free GB"]
+        assert dataclass.total_gb == function["total GB"]
 
 def test_generate_drive_registry(paths=configuration.drive_paths):
     """Does the dataclass based function generate_drive_registry fulfil the same function as create_disks_list?"""
