4da5c740bbc48bf16ea402036eeb750855827843
da-tra <dtraska@gmail.com>
2024-11-26 12:53:01 +0100

    finish test function for refactored FUN check_disk_usage_dc

--- test_main.py
+++ test_main.py
@@ -5,11 +5,12 @@ import configuration
 def test_dclass_diskusageinfo(path):
     dataclass = check_disk_usage_dc(path)
     function = check_disk_usage(path)
-    
-    print(dataclass)
-    print(function)
+
+    assert dataclass.used_percent == function["used %"]
+    assert dataclass.free_gb == function["free GB"]
+    assert dataclass.total_gb == function["total GB"]
 
 def test_create_table_html():
     """Does the HTML table created from dataclass DriveInfo match the one created from dictionaries?"""
 
-test_dclass_diskusageinfo(configuration.drive_paths[0])
+# test_dclass_diskusageinfo(configuration.drive_paths[0])
