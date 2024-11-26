61964275dc33285fda48304adc0867b4b98eb742
da-tra <dtraska@gmail.com>
2024-11-26 13:01:12 +0100

    gather todos  for after  refactoring to dataclass base

--- test_main.py
+++ test_main.py
@@ -2,13 +2,16 @@ from main import DriveInfo, DiskUsageInfo, check_disk_usage
 from main import create_disks_list, create_table_html, check_disk_usage_dc
 import configuration
 
-def test_dclass_diskusageinfo(path):
+def test_dclass_diskusageinfo(path=configuration.drive_paths[0]):
+    """Does the dataclass based check_disk_usage_dc obtain the same data as check_disk_usage?"""
     dataclass = check_disk_usage_dc(path)
     function = check_disk_usage(path)
 
     assert dataclass.used_percent == function["used %"]
     assert dataclass.free_gb == function["free GB"]
     assert dataclass.total_gb == function["total GB"]
+
+
 
 def test_create_table_html():
     """Does the HTML table created from dataclass DriveInfo match the one created from dictionaries?"""
