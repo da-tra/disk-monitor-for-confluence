62f87e6590ca888b969ef67537c8f541f502bf47
da-tra <dtraska@gmail.com>
2024-11-27 12:27:21 +0100

    finish fixing test function for dataclass based list of drive capacity info

--- test_main.py
+++ test_main.py
@@ -23,13 +23,30 @@ def test_generate_drive_registry(paths: list =configuration.drive_paths):
     #     disks_list = create_disks_list(drive)
     #     assert registry[index].path == disks_list[index]["path"]
     #     index += 1
-    index = 0
-    registry = create_drive_registry(paths)[0].storage.used_percent
-    disks_list = create_disks_list(paths)[0]["storage"]["used %"]
-    print(registry)
-    print(disks_list)
+    
+    for index in range(len(paths)):
+        registry = create_drive_registry(paths)[index].path
+        disks_list = create_disks_list(paths)[index]["path"]
+        assert registry == disks_list
 
-def test_create_table_html():
-    """Does the HTML table created from dataclass DriveInfo match the one created from dictionaries?"""
+        registry = create_drive_registry(paths)[index].storage.used_percent
+        disks_list = create_disks_list(paths)[index]["storage"]["used %"]
+        assert registry == disks_list
+
+        registry = create_drive_registry(paths)[index].storage.free_gb
+        disks_list = create_disks_list(paths)[index]["storage"]["free GB"]
+        assert registry == disks_list
+
+        registry = create_drive_registry(paths)[index].storage.total_gb
+        disks_list = create_disks_list(paths)[index]["storage"]["total GB"]
+        assert registry == disks_list
+
+        registry = create_drive_registry(paths)[index].storage.time_of_snapshot
+        disks_list = create_disks_list(paths)[index]["storage"]["time of snapshot"]
+        assert registry == disks_list
+
+
+# def test_create_table_html():
+#     """Does the HTML table created from dataclass DriveInfo match the one created from dictionaries?"""
 
 test_generate_drive_registry()
