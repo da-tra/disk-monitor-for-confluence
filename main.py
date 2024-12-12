029f243cdea869b98a4e92f513684d54d5d6ef01
da-tra <dtraska@gmail.com>
2024-12-12 13:39:12 +0100

    reformat section headlines (code comments)

--- main.py
+++ main.py
@@ -202,8 +202,10 @@ def send_warning_email(warnings_count: int, capacity_limit: float, warning_text:
             msg=em.as_string(),
         )
 
+###########################
+# Confluence-page  update #
+###########################
 
-####### Confluence page  update ######
 
 # import information about your Confluence page from the file configuration.py
 CONFLUENCE_PAGE_ID = configuration.confluence_page_id
@@ -232,7 +234,10 @@ update_page_with_new_content(
     new_content=table,
     existing_version=version_number_before_update)
 
-########## SQLite logging #########
+##################
+# SQLite logging #
+##################
+
 
 # Add rows of data to the table
 # example:
@@ -289,7 +294,10 @@ for drive in drives:
 # Close the connection to the database
 conn.close()
 
-########## Email notification ##########
+######################
+# Email notification #
+######################
+
 
 warnings_count, warning_content = check_for_critical_capacity(
     capacity_limit=configuration.capacity_limit,
